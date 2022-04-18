3b00828f04588bfda465a62e13279952c450138a
Ingrid Larissa <larissaorcx@gmail.com>
2022-04-18 00:14:03 -0300

    Adicionando os ícones ao header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,41 @@
+import Image from 'next/image';
+import style from './header.module.scss';
+
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <>
+      <div className={style.header}>
+        <div className={style.itens}>
+          <div className={style.menu}>
+            <button type="button">
+              <Image
+                alt="menu-categorias"
+                src="/menu.png"
+                width={42}
+                height={26}
+              />
+            </button>
+          </div>
+          <div className={style.logo}>
+            <Image
+              alt="logo"
+              src="/Logo-Lunettes.png"
+              width={245.3}
+              height={108.4}
+            />
+          </div>
+          <div className={style.sacola}>
+            <button type="button">
+              <Image
+                alt="sacola"
+                src="/sacola.png"
+                width={58.8}
+                height={55.7}
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
