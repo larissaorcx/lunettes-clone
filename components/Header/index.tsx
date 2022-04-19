509c1272ba758ea75e3a98a17f594eb5117e52b5
Ingrid Larissa <larissaorcx@gmail.com>
2022-04-19 00:47:45 -0300

    Arrumando o background img

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,35 +5,28 @@ export default function Header() {
   return (
     <>
       <div className={style.header}>
-        <div className={style.itens}>
-          <div className={style.menu}>
-            <button type="button">
-              <Image
-                alt="menu-categorias"
-                src="/menu.png"
-                width={42}
-                height={26}
-              />
-            </button>
-          </div>
-          <div className={style.logo}>
+        <div className={style.menu}>
+          <button type="button">
             <Image
-              alt="logo"
-              src="/Logo-Lunettes.png"
-              width={245.3}
-              height={108.4}
+              alt="menu-categorias"
+              src="/menu.png"
+              width={42}
+              height={26}
             />
-          </div>
-          <div className={style.sacola}>
-            <button type="button">
-              <Image
-                alt="sacola"
-                src="/sacola.png"
-                width={58.8}
-                height={55.7}
-              />
-            </button>
-          </div>
+          </button>
+        </div>
+        <div className={style.logo}>
+          <Image
+            alt="logo"
+            src="/Logo-Lunettes.png"
+            width={245.3}
+            height={108.4}
+          />
+        </div>
+        <div className={style.sacola}>
+          <button type="button">
+            <Image alt="sacola" src="/sacola.png" width={58.8} height={55.7} />
+          </button>
         </div>
       </div>
     </>
