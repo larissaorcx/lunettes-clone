ab83dd0152c3eadc61674237a903ef6c0f81034d
Ingrid Larissa <larissaorcx@gmail.com>
2022-04-20 00:51:30 -0300

    Adicionando o passo a passo do atendimento

--- components/Contatos/index.tsx
+++ components/Contatos/index.tsx
@@ -4,7 +4,7 @@ import style from './contatos.module.scss';
 export default function Contatos() {
   return (
     <>
-      <div className={style.content}>
+      <div className={style.atendimento}>
         <Image
           alt="contacts-img"
           src="/contatos-img.png"
@@ -17,6 +17,38 @@ export default function Contatos() {
             A Lunettes by Lari é uma ótica que leva seus óculos na porta da sua
             casa. Nosso atendimento é uma consultoria de estilo exclusiva.
           </p>
+        </div>
+      </div>
+      <div className={style.contatos}>
+        <h2>Como funciona?</h2>
+
+        <div className={style.infosContacts}>
+          <div className={style.info1}>
+            <Image alt="coracao" src="/coracao.png" width={80.4} height={72} />
+
+            <span>
+              <strong>Escolha</strong>
+              Você escolhe seus óculos por aqui
+            </span>
+          </div>
+
+          <div className={style.info2}>
+            <Image alt="whats" src="/whats.png" width={72.6} height={73.3} />
+
+            <span>
+              <strong>Avise a Lunettes</strong>
+              Avise a Lunettes quais óculos quer experimentar
+            </span>
+          </div>
+
+          <div className={style.info3}>
+            <Image alt="agenda" src="/agenda.png" width={78.2} height={72.2} />
+
+            <span>
+              <strong>Agende</strong>
+              Vamos agendar uma visita e levar os óculos até você
+            </span>
+          </div>
         </div>
       </div>
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.googleapis.com" />
 
             <link
-              href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200&display=swap"
+              href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200;600&display=swap"
               rel="stylesheet"
             />
           </Head>
