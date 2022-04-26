d2fbba0d2d283a7a602ddb35140bf46a68f57179
Ingrid Larissa <larissaorcx@gmail.com>
2022-04-26 00:49:27 -0300

    Adicionando um destaque estático

--- components/Destaques/index.tsx
+++ components/Destaques/index.tsx
@@ -1,9 +1,28 @@
 import style from './destaques.module.scss';
+import Image from 'next/image';
 
 export default function Destaques() {
   return (
     <div className={style.destaques}>
       <h2>Prodrutos em destaque</h2>
+      <div className={style.produto}>
+        <Image alt="foto1" src="/foto1.png" width={360} height={360} />
+        <div className={style.cores}>
+          <button type="button"></button>
+          <button type="button"></button>
+        </div>
+        <div className={style.infosProdutos}>
+          <h3>CLASSIC LINE</h3>
+          <p>CLL980</p>
+        </div>
+        <div className={style.price}>
+          <span>R$ 120,00</span>
+        </div>
+        <button type="button">
+          <Image alt="iconeBotão" src="/ibotao.png" width={33} height={30} />
+          <span>Reservar</span>
+        </button>
+      </div>
     </div>
   );
 }
