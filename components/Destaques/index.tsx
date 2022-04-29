7d508d2deecdf07b703087c0f02923e3aa71271e
Ingrid Larissa <larissaorcx@gmail.com>
2022-04-29 01:56:41 -0300

    Adicionando styled components no Destaques

--- components/Destaques/index.tsx
+++ components/Destaques/index.tsx
@@ -1,32 +1,39 @@
-import style from './destaques.module.scss';
 import Image from 'next/image';
-
 import { FaRegHeart } from 'react-icons/fa';
+
+import {
+  DestaquesContainer,
+  Titulo,
+  InfosProduto,
+  NomeProduto,
+  ProdutoContainer,
+  CodigoProduto,
+  Preço,
+  BotaoReserva,
+  TextoBotao,
+  BotaoCores,
+  CoresContainer,
+} from './style';
 
 export default function Destaques() {
   return (
-    <div className={style.destaques}>
-      <h2>Prodrutos em destaque</h2>
-      <div className={style.produto}>
+    <DestaquesContainer>
+      <Titulo>Prodrutos em destaque</Titulo>
+      <ProdutoContainer>
         <Image alt="foto1" src="/foto1.png" width={360} height={360} />
-        <div className={style.opcaoesCores}>
-          <button type="button" className={style.color1}></button>
-          <button type="button" className={style.color2}></button>
-        </div>
-        <div className={style.infosProdutos}>
-          <h3>CLASSIC LINE</h3>
-          <p>CLL980</p>
-        </div>
-        <div className={style.price}>
-          <span>R$ 120,00</span>
-        </div>
-        <div className={style.reserva}>
-          <button type="button">
-            <FaRegHeart />
-            <span>Reservar</span>
-          </button>
-        </div>
-      </div>
-    </div>
+        <CoresContainer>
+          <BotaoCores type="button"></BotaoCores>
+        </CoresContainer>
+        <InfosProduto>
+          <NomeProduto>CLASSIC LINE</NomeProduto>
+          <CodigoProduto>CLL980</CodigoProduto>
+        </InfosProduto>
+        <Preço>R$ 120,00</Preço>
+        <BotaoReserva type="button">
+          <FaRegHeart />
+          <TextoBotao>Reservar</TextoBotao>
+        </BotaoReserva>
+      </ProdutoContainer>
+    </DestaquesContainer>
   );
 }
---
+++ components/Destaques/style.ts
@@ -0,0 +1,101 @@
+import styled from 'styled-components';
+
+export const DestaquesContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+
+  flex-direction: column;
+`;
+
+export const Titulo = styled.h2`
+  font-family: MonumentExtended;
+  text-transform: uppercase;
+  font-size: 42px;
+  font-weight: 800;
+  padding-top: 70px;
+`;
+export const ProdutoContainer = styled.div`
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+export const InfosProduto = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  padding-bottom: 25px;
+`;
+export const NomeProduto = styled.h3`
+  font-family: 'Roboto Mono', monospace;
+  font-size: 24px;
+
+  line-height: 1.5;
+  color: #000;
+  margin-top: 10px;
+`;
+export const CodigoProduto = styled.p`
+  font-family: 'Roboto Mono', monospace;
+  font-size: 16px;
+
+  line-height: 1.25;
+  color: #9a9a9a;
+`;
+export const Preço = styled.span`
+  font-family: 'Roboto Mono', monospace;
+  font-size: 24px;
+  font-weight: bold;
+
+  line-height: 1.5;
+  color: black;
+`;
+export const BotaoCores = styled.button`
+  width: 32px;
+  height: 32px;
+  border: none;
+  border-radius: 4px;
+  margin: 10px 20px 0px 0px;
+  cursor: pointer;
+  background-color: #7c1c36;
+
+  margin-top: 15px;
+
+  &:hover {
+    border-radius: 8px;
+    border: solid 2px #000;
+  }
+`;
+export const BotaoReserva = styled.button`
+  width: 248px;
+  height: 70px;
+  background: black;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.3);
+  font-family: 'Roboto Mono', monospace;
+  font-size: 24px;
+
+  line-height: 1.5;
+  color: #fff;
+  border: none;
+  margin: 20px 0px 70px 0px;
+  border-radius: 8px;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const TextoBotao = styled.span`
+  margin-left: 13px;
+`;
+export const CoresContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
